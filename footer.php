bd8cc773a1d67577e281f1ce793b359b4162b309
Pascual27 <115171634+Pascual27@users.noreply.github.com>
2023-04-19 09:57:44 -0500

    mi primera plantilla

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer class="bg-dark text-white text-center py-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <p>&copy; WILLY CF - PROHIBIDO COPIARSE O SERA SENTENCIADO A UNA PENA PRIVATIVA DE CARCEL</p>
+        </div>
+      </div>
+    </div>
+  </footer>
---
+++ index.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pagina web WILLY</title>
+   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  </head>
+  
+<body>
+
+<?php include('menu.php'); ?>
+
+
+<div class="container my=3">
+    <center>
+     <h1>WILLY CF</h1> <br>
+</center>
+     <div class="row">
+
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/buho.jpg" alt="Imagen 1" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">buho1</h5>
+             <p class="card-text">La vida no es fácil y siendo fácil no es vida porque sin ser difícil entonces no hay vida, lo difícil de la vida es la muerte y sin vida no hay muerte por eso sin ser difícil no hay vida.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/buho.jpg" alt="Imagen 2" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">buho2</h5>
+             <p class="card-text">La vida no es fácil y siendo fácil no es vida porque sin ser difícil entonces no hay vida, lo difícil de la vida es la muerte y sin vida no hay muerte por eso sin ser difícil no hay vida.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+      
+      
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/buho.jpg" alt="Imagen 3" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">buho3</h5>
+             <p class="card-text">La vida no es fácil y siendo fácil no es vida porque sin ser difícil entonces no hay vida, lo difícil de la vida es la muerte y sin vida no hay muerte por eso sin ser difícil no hay vida.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+        </div>
+        
+       
+     
+     
+    
+      <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/buho.jpg" alt="Imagen 4" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">buho4</h5>
+             <p class="card-text">La vida no es fácil y siendo fácil no es vida porque sin ser difícil entonces no hay vida, lo difícil de la vida es la muerte y sin vida no hay muerte por eso sin ser difícil no hay vida.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+      
+       
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/buho.jpg" alt="Imagen 5" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">buho5</h5>
+             <p class="card-text">La vida no es fácil y siendo fácil no es vida porque sin ser difícil entonces no hay vida, lo difícil de la vida es la muerte y sin vida no hay muerte por eso sin ser difícil no hay vida.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+       
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="img/buho.jpg" alt="Imagen 6" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">buho6</h5>
+             <p class="card-text">La vida no es fácil y siendo fácil no es vida porque sin ser difícil entonces no hay vida, lo difícil de la vida es la muerte y sin vida no hay muerte por eso sin ser difícil no hay vida.</p>
+             <a href="#" class="btn btn-primary">Comprar</a>
+             <a href="#" class="btn btn-primary">Añadir al carrito</a>
+           </div>
+         </div>
+       </div>
+       </div> 
+       </div>
+       
+       
+       <br>
+      
+
+       <?php include('footer.php'); ?>
+ 
+       
+  
+      <script src="bootstrap/js/bootstrap.min.js"></script>
+
+
+
+
+</body>
